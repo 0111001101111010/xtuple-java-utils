acc940d5979ef6ead9d140c46dc253f06f77c800
stanzheng <dev.stanzheng@gmail.com>
2014-03-31 17:05:51 -0400

    scaffolding

---
+++ src/main/java/com/xtuple/XtupleRestClient.java
@@ -0,0 +1,20 @@
+package com.xtuple;
+
+import java.io.*;
+import org.json.*;
+import java.util.*;
+
+public class XtupleRestClient{
+
+    public static void main(String[] args)
+    {
+    try{
+    	JSONObject foo = new JSONObject("{}");
+    }
+    catch(Exception e){
+
+    }
+
+    }
+
+}
