abf2c57542260673308efb1fce921d2886762672
stanzheng <dev.stanzheng@gmail.com>
2014-04-12 17:25:05 -0400

    testing issue to shipment

--- src/test/java/com/xtuple/XtupleRestClientTest.java
+++ src/test/java/com/xtuple/XtupleRestClientTest.java
@@ -187,8 +187,8 @@ public class XtupleRestClientTest
     System.out.println("End of of testShippable~2");
     }
     //test DispatchIssue
-    public void testIssueOrder(){
-    System.out.println("Start of testIssueOrder~2");
+    public void testDispatchIssueToShipping(){
+    System.out.println("Start of testDispatchIssueToShipping~2");
         HashMap<String, String> info = new HashMap<String, String>();
         info.put("uuid", "0e6fd01e-7dd2-41f0-dd0e-6cad09078fb3");
         info.put("quantity", "1");
@@ -200,13 +200,13 @@ public class XtupleRestClientTest
         String words = asyncHttpPost.getWords();
         System.out.println(words);
         //asyncHttpPost.execute("http://192.168.10.53:8081/orders");
-    System.out.println("End of testIssueOrder~2");
+    System.out.println("End of testDispatchIssueToShipping~2");
     }
     //test DispatchShipment
-    public void testIssueShipment(){
-    System.out.println("Start of IssueShipment~2");
+    public void testDispatchShipment(){
+    System.out.println("Start of testDispatchShipment~2");
         HashMap<String, String> info = new HashMap<String, String>();
-        info.put("shipID", "65012");
+        info.put("shipID", "60176");
         Post asyncHttpPost = new Post(info);
         //#Work
         //asyncHttpPost.execute("http://192.168.10.53:8081/orders");
@@ -215,7 +215,7 @@ public class XtupleRestClientTest
         String words = asyncHttpPost.getWords();
         System.out.println(words);
         //asyncHttpPost.execute("http://192.168.10.53:8081/orders");
-    System.out.println("End of IssueShipment~2");
+    System.out.println("End of testDispatchShipment~2");
     }
     //issue a issueToShipping IF a barcode is matched
     /**
